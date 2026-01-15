acc6904fe67e2a8549ce8e17522d20578eab1749
Kyle Barron <kyle@developmentseed.org>
2026-01-15 16:38:01 -0500

    feat: Mosaic tile layer (#184)
    ### Change list
    
    - Add `MosaicLayer` deck.gl layer that renders multiple items based on
    what's on screen. Under the hood it uses a TileLayer
    - Allow `AbortSignal` input into `COGLayer`
    - New NAIP mosaic example: 
    - Render either true color, false color infrared, or NDVI with colormap
        - Define proj strings in app for bundle size
        - 
    
    
    cc @gadomski

--- packages/deck.gl-geotiff/src/index.ts
+++ packages/deck.gl-geotiff/src/index.ts
@@ -9,4 +9,10 @@ export {
   extractGeotiffReprojectors,
   fromGeoTransform,
 } from "./geotiff-reprojection.js";
+export type { MosaicLayerProps } from "./mosaic-layer/mosaic-layer.js";
+export { MosaicLayer } from "./mosaic-layer/mosaic-layer.js";
+export {
+  type MosaicSource,
+  MosaicTileset2D,
+} from "./mosaic-layer/mosaic-tileset-2d";
 export * as proj from "./proj.js";
